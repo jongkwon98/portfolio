ee93851ace16f47763a730e187a09a04d7b4e13a
jongkwon Park <jk980502@naver.com>
2023-02-09 17:39:11 +0900

    JS Navbar menu toggle + Project UI

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ document.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove('navbar--dark');
   }
+  navbarMenu.classList.remove('open');
 });
 
 // Handle scrolling when tapping on the navbar menu
@@ -16,10 +17,17 @@ const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
-  if (link === null) {
+  if (link == null) {
     return;
   }
+  s;
   scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
 // Handle click on "contact me" button on home
